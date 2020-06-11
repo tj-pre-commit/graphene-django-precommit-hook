af8f385fea8d9d4132feb56ea32e65e0fb6f018d
Tonye Jack <tonye@coherentpath.com>
2020-06-11 19:05:56 -0400

    Updated the main handler to pass other arguments.

--- graphene_django_hook.py
+++ graphene_django_hook.py
@@ -29,14 +29,30 @@ def run_command(command: str) -> int:
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument('--python-version', default='3.6')
+    parser.add_argument('--indent', default=None)
+    parser.add_argument('--out', default=None)
+    parser.add_argument('--schema', default=None)
+    parser.add_argument('-v', '--verbosity', default=None)
     parser.add_argument('--managepy-path', default='manage.py')
     parser.add_argument('--settings', default=None)
     args = parser.parse_args(argv)
     
-    command = 'python{} manage.py graphql_schema'.format(args.python_version)
+    command = 'python{} {} graphql_schema'.format(args.python_version, args.managepy_path)
     
-    if args.settings:
+    if args.settings is not None:
         command += ' --settings={}'.format(args.settings)
+
+    if args.indent is not None:
+        command += ' --indent={}'.format(args.indent)
+
+    if args.out is not None:
+        command += ' --out={}'.format(args.out)
+
+    if args.indent is not None:
+        command += ' --schema={}'.format(args.schema)
+
+    if args.verbosity is not None:
+        command += ' --verbosity={}'.format(args.verbosity)
   
     return run_command(command)
    
