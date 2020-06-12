ebf93f092e883d0b910427ef254fc5df8f325abf
Tonye Jack <tonye@coherentpath.com>
2020-06-12 00:33:03 -0400

    Fixed command.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-if [[ $(git ls-files -z -o --exclude-standard | wc -l) -eq 1 ]]; then
- echo "Unstaged changes" && exit 1;
-else
- python manage.py graphql_schema;
-fi
+has_missing() {
+  if [[ $(git ls-files -z -o --exclude-standard | wc -l) -eq 1 ]]; then
+    echo "Unstaged changes" && exit 1;
+  fi
+}
+
+has_missing
+python manage.py graphql_schema;
+has_missing
