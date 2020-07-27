b224997e3513b5bc1ed0e463eb34e82500713565
Tonye Jack <tonye@coherentpath.com>
2020-07-27 06:14:55 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 
-has_missing() {
-  if [[ $(git ls-files -z -o --exclude-standard | wc -l) -eq 1 ]]; then
-    echo "Unstaged changes" && exit 1;
-  fi
-}
-
-has_missing
 python manage.py graphql_schema
-has_missing
